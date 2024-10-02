665a0cbd879a9d5ae9f43dd9e4ea2f75306b3d07
prachi singh <prachisingh7921@gmail.com>
2024-10-03 01:57:29 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I've been a passionate full-stack developer since the age of 13, with a love for coding and building impactful web applications. Over the past 5 years, I’ve honed my expertise in front-end technologies like React and Next.js, and back-end tools such as Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to use my skills to develop efficient, scalable solutions that support business growth and deliver seamless user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Karmyug Tech Zone",
+    description: `As an intern at Karmyug Tech Zone, I actively contribute to organizing various tech workshops and training sessions aimed at students. My responsibilities include developing websites and managing front-end development tasks, ensuring the smooth execution of our digital projects. Additionally, I work on creating detailed data analysis reports to highlight job trends and career opportunities in the tech industry, helping students make informed career decisions.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2021 - 2023",
+    role: "Frontend Developer",
+    company: "Self-Employed",
+    description: `During this period, I worked as a freelance web developer, where I designed and built websites for local businesses and individual clients. Using technologies like HTML, CSS, JavaScript, and React, I provided customized solutions that helped clients establish their online presence. I was involved in creating dynamic, responsive websites and handling projects such as e-commerce platforms and personal portfolio websites. These experiences helped me refine my development skills and deliver high-quality technical solutions.`,
+    technologies: ["HTML", "CSS", "React", "Figma"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Tech Club Organizer",
+    company: "School Coding Club",
+    description: `In this role, I led the school’s coding club, where I took the initiative to organize coding challenges and tech discussions for fellow students. I also provided mentorship to help students build small tech projects such as websites and basic apps. To foster collaboration and creativity, I organized a school-wide coding hackathon, which received an enthusiastic response and showcased students' potential in creating innovative solutions.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2019 - 2020",
+    role: "Student Website Developer",
+    company: "School Cultural Event",
+    description: `In this role, I designed and maintained the official website for my school's cultural event. Using HTML, CSS, and JavaScript, I created an interactive platform for event registration and showcasing highlights. I also integrated media galleries and managed the site's functionality to ensure smooth user experience throughout the event.`,
+    technologies: ["HTML", "CSS", "PHP", "Javascript"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "64 Devra Dev Colony, Ratlam, MP 457001 ",
+  phoneNo: "+91 7879285574",
+  email: "Krishnasingh2007m@gmail.com",
+};
