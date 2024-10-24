5d7ea2d4f9a50e16f3fad0a573678e4bca989246
Krishna Singh <krishnasingh2007m@gmail.com>
2024-10-24 21:55:41 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     <div className="m-8 flex items-center justify-center gap-4 text-2xl ">
         <a href="https://www.linkedin.com/in/krishna-singh-5b4402253/" target={"blank"}><FaLinkedin /></a>
         <a href="https://github.com/KrishTechverse" target={"blank"}><FaGithub /></a>
-        <a href={resume} target={"blank"}><TbFileCv /></a>
+        <a href={resume} download target={"blank"}><TbFileCv /></a>
         <a href="https://x.com/Krish_byte" target={"blank"}><FaSquareXTwitter /></a>
     </div>
   </nav>
