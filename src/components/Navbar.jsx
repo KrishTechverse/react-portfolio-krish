bc23769bbd479d56c4232288d5f39ed3086e3696
Krishna Singh <krishnasingh2007m@gmail.com>
2024-10-24 21:53:35 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { TbFileCv } from "react-icons/tb";
+import resume from "../assets/Krishna Singh - Full Stack Developer.pdf";
 
 const Navbar = () => {
   return (
@@ -13,7 +14,7 @@ const Navbar = () => {
     <div className="m-8 flex items-center justify-center gap-4 text-2xl ">
         <a href="https://www.linkedin.com/in/krishna-singh-5b4402253/" target={"blank"}><FaLinkedin /></a>
         <a href="https://github.com/KrishTechverse" target={"blank"}><FaGithub /></a>
-        <a href="https://app.rezi.ai/s/vYM6SUKrym8MybvjyWB0" target={"blank"}><TbFileCv /></a>
+        <a href={resume} target={"blank"}><TbFileCv /></a>
         <a href="https://x.com/Krish_byte" target={"blank"}><FaSquareXTwitter /></a>
     </div>
   </nav>
